e49b221b1b99c03dc3dd3ee472c20c038b489de6
Greg Schuman <gcschuman@hotmail.com>
2023-06-26 12:32:15 -0700

    Added EmptyList component and added a filler image.

---
+++ src/components/EmptyList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../index.css';
+
+const EmptyList = () => (
+    <div className='emptyList-wrap'>
+        <img src='src/assets/wangfire.jpeg' alt='empty'></img>
+    </div>
+);
+
+export default EmptyList
